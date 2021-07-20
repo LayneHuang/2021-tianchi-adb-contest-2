54e1276ed2624fa33b62a2ad3210014ffd6a0fd3
LayneHuang <hl_mark@163.com>
2021-07-20 15:04:46 +0800

    搞一发

--- src/main/java/com/aliyun/adb/contest/SimpleAnalyticDB.java
+++ src/main/java/com/aliyun/adb/contest/SimpleAnalyticDB.java
@@ -68,16 +68,12 @@ public class SimpleAnalyticDB implements AnalyticDB {
         }
     }
 
-    private int debug = 1;
-
     @Override
     public String quantile(String table, String column, double percentile) throws IOException {
-        if (debug > 10) return "0";
-        debug++;
         int tIdx = indexMap.get(table);
         int colIdx = tables[tIdx].colIndexMap.get(column);
         long ans = MyPageManager.find(tables[tIdx], tIdx, colIdx, percentile);
-        System.out.println("query: " + table + ", column: " + column + ", ans: " + ans);
+        // System.out.println("query: " + table + ", column: " + column + ", percentile:" + percentile + ", ans:" + ans);
         return String.valueOf(ans);
     }
 
