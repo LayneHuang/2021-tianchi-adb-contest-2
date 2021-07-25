168ff7de3f74823d7c36add18cea1d9893ff25ce
laynehuang <hl_mark@163.com>
2021-07-26 01:44:28 +0800

    搞事情

--- src/main/java/com/aliyun/adb/contest/pool/WriteThread.java
+++ src/main/java/com/aliyun/adb/contest/pool/WriteThread.java
@@ -1,18 +1,19 @@
 package com.aliyun.adb.contest.pool;
 
-import com.aliyun.adb.contest.Constant;
 import com.aliyun.adb.contest.cache.MyBlockingQueueCache;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WriteThread extends Thread {
 
     public MyBlockingQueueCache bq = new MyBlockingQueueCache();
 
-    private boolean[][] written = new boolean[Constant.PAGE_COUNT][2];
+    private Set<String> st = new HashSet<>();
 
     @Override
     public void run() {
@@ -25,9 +26,9 @@ public class WriteThread extends Thread {
                     task.getPath(),
                     StandardOpenOption.WRITE,
                     StandardOpenOption.CREATE,
-                    written[task.getPIdx()][task.getCIdx()] ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING
+                    st.contains(task.getPath().toString()) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING
             )) {
-                written[task.getPIdx()][task.getCIdx()] = true;
+                st.add(task.getPath().toString());
                 ByteBuffer buffer = task.getBuffer();
                 buffer.flip();
                 fileChannel.write(buffer);
