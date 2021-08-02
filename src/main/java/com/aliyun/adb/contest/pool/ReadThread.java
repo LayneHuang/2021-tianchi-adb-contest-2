2e6a8843167b374b9241aee252e5e1ae56ece2e2
LayneHuang <hl_mark@163.com>
2021-08-02 17:49:34 +0800

    单位转换有点问题

--- src/main/java/com/aliyun/adb/contest/pool/ReadThread.java
+++ src/main/java/com/aliyun/adb/contest/pool/ReadThread.java
@@ -104,10 +104,10 @@ public class ReadThread extends Thread {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("transBeginT: " + (transBeginT / 1000)
-                + "(ms), transT: " + (transT / 1000)
-                + "(ms), divPageT: " + (divPageT / 1000)
-                + "(ms), submitT: " + (submitT / 1000) + "(ms)");
+        System.out.println("transBeginT: " + (transBeginT / 1000000)
+                + "(ms), transT: " + (transT / 1000000)
+                + "(ms), divPageT: " + (divPageT / 1000000)
+                + "(ms), submitT: " + (submitT / 1000000) + "(ms)");
     }
 
     private void noTrans(MappedByteBuffer buffer) {
